cc9156e5b5f6a25ab262328e694f094bd788f2ac
Leyvi Ni <llskyhi@gmail.com>
2026-08-03 16:06:51 +0800

    Add OpenBao userpass auth method
    Before this commit, the application was using dev root token to
    authenticate in OpenBao, thus it did rely on the root policy - all
    operation are allowed.
    This commit adds another auth method that is granted with a limited
    policy, which is closer to real-world application uses.

---
+++ init/openbao/021-userpass.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# Enable the "userpass" auth method, create a user
+# https://openbao.org/docs/auth/userpass/#configuration
+
+policy_name='sats-policy'
+
+userpass_mount_path='sats-userpass'
+userpass_username='sats-user'
+userpass_password='sats-pass'
+
+bao auth enable \
+    -path="$userpass_mount_path" \
+    userpass
+
+bao write \
+    "auth/$userpass_mount_path/users/$userpass_username" \
+    password="$userpass_password" \
+    policies="$policy_name"
